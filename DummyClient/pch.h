8a62ba3b11f7a5f9c2332eb4942ca372ee7d3c49
unknown <tkdlqldjq123@gmail.com>
2024-05-28 23:43:44 +0900

    init

---
+++ DummyClient/pch.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifdef _DEBUG
+#pragma comment(lib, "Debug\\ServerCore.lib")
+#else
+#pragma comment(lib, "Release\\ServerCore.lib")
+#endif
+
+#include "CorePch.h" //포함디렉토리
---
+++ ServerCore/CorePch.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+
+void HelloGameServer()
+{
+	cout << "Hello GameServer!" << std::endl;	
+}
+
+void HelloDummyClient()
+{
+	cout << "Hello DummyClient!" << std::endl;
+}
