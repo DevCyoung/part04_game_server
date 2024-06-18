59ddd66b38b06011231382d4b26b8bbe7ed4d9e6
DevCYoung <tkdlqldjq123@gmail.com>
2024-06-18 23:28:49 +0900

    futrue

--- GameServer/main.cpp
+++ GameServer/main.cpp
@@ -4,74 +4,104 @@
 #include <mutex>
 #include <Windows.h>
 #include <assert.h>
+#include <future>
 
-//이벤트라는것의 응용
+int64 result = 0;
 
-mutex m;
-queue<int32> q;
-HANDLE handle;
-
-//참고) CV는 User-LEvel Object (커널오브젝트X)
-condition_variable cv; //이거슨표준 마음껏사용해도된다. 추천되는 좋은방법임
-
-void Producer()
+void PromiseWorker(std::promise<string>&& promise)
 {
-	//1 Lock을 잡는다.
-	//2 공유 변수 값을 수정
-	//3 Lock을 풀고
-	//4 조건변수통해 다른 쓰레드에게 통지
-	while (true)
-	{
-		{
-			unique_lock<mutex> lcok(m);
-			q.push(100);
-		}		
-
-
-		cv.notify_one(); //waitw중인 쓰레드가있다면 딱 1개를 꺠운다.
-	}
+	promise.set_value("sevret message");
 }
 
-void Consumer()
+void TeaskWorker(std::packaged_task<int64(void)>&& task)
 {
-	while (true)
-	{		
-		unique_lock<mutex> lock(m);
-		cv.wait(lock, []() {return q.empty() == false; });
-		// 1 Lock을잡고 (지금은 이미잡혀있음 잡혔으면통과 안잡혔으면 잡으려고시도)
-		// 2 조건확인
-		// 3 만족하면 빠져서나와서 이어서 코드를 진행
-		// 4 만족하지않는다 (Lock을 풀어주고 대기상태로 만든다) 락가드로는안됨 유닉락으로만 사용해야함
-		// 조건을 만족하지않으면 락을풀고 wait를한다.
-		// wait중인상태에서 cv.notify_one() 빵하고 락을잡고 조건을확인이걸다시진행
+	task();
+}
 
-		// 그런데 Notyfi_one을 했으면 항상 조건식을 만족하는건 아닐까?
-		// Spurius Wakeup 가짜기상??
-		// notify_one을 할때 lock을 잡고 있는것이 아니기때문에
-
-		int32 data = q.front();
-		q.pop();
-		cout << data << endl;
-
+//뭔가복잡한계산
+int64 Calculate()
+{
+	int sum = 0;
+	for (int32 i = 0; i < 1000'000; i++)
+	{
+		sum += i;
 	}
+
+	result = sum;
+
+	return sum;
 }
 
 int main()
 {
-	//FALSE 여야 오토리셋이다
-	//FALSE 로둬서 시그널을끈다.
-	//커널에서 만들어주는 커널오브젝트
-	//Usage Count
-	//Singnal(파란불), Non-Signal(빨간불) (커널오브젝트가 가진 공통적인속성)	
-	handle =  ::CreateEvent(NULL/*보안속성*/, FALSE, FALSE, NULL);
+	// 동기( synchronous) 실행
+	//int64 sum = Calculate();
+
+	//만약 Caculate가 정말 오래걸리는 함수라면?
+	//비동기방식이 유용해진다!
+
+	//기존방법
+	//thread t(Calculate);
+	//t.join();
+
+	//std::future 사용예제
+	{
+		//1) deferred -> lazy evaluation 지연해서 실행하세요 (그냥동기) 커맨드패턴의 전형적인 패턴 실행시점이 뒤로밀림 어떻게보면 비동기임 ㅋㅋ
+		//2) async -> 별도의 쓰레드를 만들어서 실행하세요 (우리가 관심을 가지는방법) (스레드를 병렬로 실행해줌)
+		//3) deferred | async -> 둘 중 알아서 골라주세요
+		std::future<int64> future = std::async(std::launch::async, Calculate); //언젠가 미래에 결과물을 뱉어줄거야
+		//get을 하는순간 결과물을 얻어올수있다고 약속
+		//스레드를 만들어서 관리하고 사용할수도 있지만 한번만사용할때 용이하다 (편하게 사용할수있음)
+		//비동기이기 때문에 무조건 리턴을함		
+		//TODO
+		//한참후에 그일이 끝났을까? 사용
+		//std::future_status status =  future.wait_for(1ms);
+
+		//future.wait(); get 이나 wait나 똑같음
+
+		int64 sum = future.get(); // 결과물이 이제서야 필요하다!
 
 
-	thread t1(Producer);
-	thread t2(Consumer);
+		//멤버함수를 호출하고 싶을때?
 
-	t1.join();
-	t2.join();
+		class Knight
+		{
+		public :
+			int64 GetHP() { return 100; }
+		};
 
-	assert(handle != 0);
-	::CloseHandle(handle);
+		Knight knight;
+		std::future<int64> future2 = std::async(std::launch::async, &Knight::GetHP, knight); //knight.GetHP()
+	}
+
+
+	//std::promise future객체를 만드는 두번째방법
+	{
+		//미래에(std::futrue)에 결과물을 반환해줄거라 약속 해줘~ 계약서?
+		//지저분하게 전역 변수를 이용하기보다는 promise로 짝을맞춰서 사용
+		std::promise<string> promise; //pending
+		std::future<string> future = promise.get_future(); //pending
+
+		thread t(PromiseWorker, std::move(promise)); //empty 더이상 건드리면안됨
+
+		string message = future.get();//empty 더이상 건드리면안됨
+		//future.get(); 
+
+		t.join();
+	}
+
+
+	//std::packaged_task
+	{
+						//반환타입,인풋타입맞춰야함
+		std::packaged_task<int64(void)> task(Calculate);
+		std::future<int64> future = task.get_future();
+
+		//결과물이 future을 통해 알수있음
+		std::thread t(TeaskWorker, std::move(task));
+
+		int64 sum = future.get();
+
+		t.join();
+	}
 }
