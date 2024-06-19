3c501dad92b7fbbded4e5dae49d3c404cf74ce54
DevCYoung <tkdlqldjq123@gmail.com>
2024-06-19 23:03:20 +0900

    CPU 파이프라인

--- GameServer/main.cpp
+++ GameServer/main.cpp
@@ -6,43 +6,58 @@
 #include <assert.h>
 #include <future>
 
-int32 buffer[10'000][10'000];
+//가시성 (코드재배치 컴파일러입장에서) 컴파일러가 하지않더라도 CPU가 뒤집일수도있다 (디스어셈블리가 그렇다하더라도 CPU가 멋대로 바꾼다)
+static int32 x = 0;
+static int32 y = 0;
+static int32 r1 = 0;
+static int32 r2 = 0;
+
+static volatile bool flag = false;
+
+void Thread_1()
+{
+	while (!flag)
+	{
+	}
+	y = 1; // Store y 여기서  y = 1을 대입한 명령어가 있다고해서 곧이 곧대로 처박는게아님 (캐시에 넣을수도있음) 이를 가시성 이라고한다.
+	r1 = x; // Load x
+}
+
+void Thread_2()
+{
+	while (!flag)
+	{
+	}
+
+	x = 1; // Store x
+	r2 = y; // Load y 여기서 로드하는게 진짜 메모리에서 로드하는게 아닐수도있음 (캐시에서로드함)
+}
 
 int main()
 {
-	memset(buffer, 0, sizeof(buffer));
+	int32 count = 0;
 
-	int arr1[3][1];
-	int arr2[3];
-
-	int32 size1 = sizeof(arr1);
-	int32 size2 = sizeof(arr2);
-
+	while (true)
 	{
-		int64 sum = 0;
-		uint64 start = GetTickCount64();
-		for (int32 i = 0; i < 10'000; ++i)
+		flag = false;
+
+		++count;
+
+		x = y = r1 = r2 = 0;
+
+		thread t1(Thread_1);
+		thread t2(Thread_2);
+
+		flag = true;
+
+		t1.join();
+		t2.join();
+
+		if (r1 == 0 && r2 == 0)
 		{
-			for (int32 j = 0; j < 10'00; ++j) 
-			{
-				sum += buffer[i][j];
-			}
+			break;
 		}
-		uint64 end = GetTickCount64();
-		cout << "Elapsed Tick 1 " << end - start << endl;
 	}
 
-	{
-		int64 sum = 0;
-		uint64 start = GetTickCount64();
-		for (int32 i = 0; i < 10'000; ++i)
-		{
-			for (int32 j = 0; j < 10'00; ++j)
-			{
-				sum += buffer[j][i];
-			}
-		}
-		uint64 end = GetTickCount64();
-		cout << "Elapsed Tick 2 " << end - start << endl;
-	}
+	cout << count << endl;
 }
