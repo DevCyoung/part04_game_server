c5eef9c1d7e267a9aaf3ec0b0ea63b0a8f17bcce
DevCYoung <tkdlqldjq123@gmail.com>
2024-10-17 00:32:56 +0900

    splin lock

--- GameServer/main.cpp
+++ GameServer/main.cpp
@@ -1,49 +1,65 @@
 #include "pch.h"
-#include <thread>
 #include <mutex>
-
-#include "AccountManager.h"
-#include "UserManager.h"
-
-void Func1()
+#include <atomic>
+class SpinLock
 {
-	for (int32 i = 0; i < 100; ++i)
+public:
+	void lock()
 	{
-		UserManager::Instance()->ProcessSave();
+		bool expected = false;
+		bool desired = true;
+
+		while (false == _locked.compare_exchange_strong(expected, desired))
+		{
+			expected = false;
+		}
+	}
+
+	void unlock()
+	{
+		_locked.store(false);
+	}
+
+private:
+	atomic<bool> _locked = false;
+};
+
+SpinLock spinLock;
+
+std::mutex m;
+int32 sum;
+
+void Add()
+{
+	for (int32 i = 0; i < 100'000; ++i)
+	{
+		lock_guard<SpinLock> guard(spinLock);
+		++sum;
 	}
 }
 
-void Func2()
+void Sub()
 {
-	for (int32 i = 0; i < 100; ++i)
+	for (int32 i = 0; i < 100'000; ++i)
 	{
-		AccountManager::Instance()->ProcessLogin();
+		lock_guard<SpinLock> guard(spinLock);
+		--sum;
 	}
 }
-#include <Windows.h>
 
-#ifdef _WIN64
-#define XM_PI 1
-#else
-#define XM_PI 2
-#endif
+void Test(volatile bool* b)
+{
+	//std::this_thread::sleep_for(10s);
+	*b = false;
+}
 
-#ifdef _DEBUG
-#endif
-#include <assert.h>
 int main()
 {	
-	std::thread t1(Func1);
-	std::thread t2(Func2);
-	//assert()
+	std::thread t1(Add);
+	std::thread t2(Sub);
+
 	t1.join();
 	t2.join();
 
-	std::cout << "Jobs Done" << std::endl;
-
-	using iiids32 = int32;
-
-	iiids32 ppap = 111;
-	//DWORD_PTR
-
+	std::cout << sum << std::endl;
 }
