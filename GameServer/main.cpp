5e8f38a80dc278af74f77cf793056d489cab0cd6
DevCYoung <tkdlqldjq123@gmail.com>
2024-10-29 23:04:48 +0900

    smart pointer

--- GameServer/main.cpp
+++ GameServer/main.cpp
@@ -10,11 +10,38 @@
 #include "ThreadManager.h"
 #include "Lock.h"
 
-CoreGlobal GCore;
+//CoreGlobal GCore;
+
+class Player
+{
+
+};
+
+//_CrtDumpMemoryLeaks();
 
 int main()
 {
-	std::unique_ptr<int32> p = make_unique<int32>(new int32);
+	using type = remove_extent_t<int32[123]>;
 
-	std::shared_ptr<int32>
+	//Shared, Weak
+	//[T*][ReCountBlocking*]
+	//조금더 우아한 방법
+	// 
+
+	// [T* | ReCountBlocking*] 같은공간에 낑겨넣는다.
+	std::shared_ptr<Player> spr3(new Player);
+	std::shared_ptr<Player> spr = make_shared<Player>();
+	std::weak_ptr<Player> wpr = spr;
+
+	//존재하는지 체크해야함
+	bool expired =  wpr.expired();
+
+	std::shared_ptr<Player> spr2 = wpr.lock(); //없으면(sharedCount가 0이면) null 상태가 됨
+	//weakCount가 0이 아니면 RefCountingBlock은 아직 안날라간다.
+
+	//[T*][ReCountBlocking*] 동일 한 레퍼런스 동일판 포인터 블럭
+	std::shared_ptr<Player> spr2(new Player);
+
+	//Shared 와 Weak는 서로 상충
+	//Weak는 Night 수명과는 연관이없다.
 }
--- ServerCore/RefCounting.h
+++ ServerCore/RefCounting.h
@@ -38,73 +38,72 @@ protected:
 * Sharedptr
 ---------------*/
 
-template<typename T>
-class TSharedPtr
-{
-public:
-	TSharedPtr() {}
-	TSharedPtr(T* ptr) { Set(ptr); }
-	
-	TSharedPtr(const TSharedPtr& rhs) { Set(rhs._ptr); }
-	TSharedPtr(TSharedPtr&& rhs) { _ptr = rhs._ptr; rhs._ptr = nullptr; }//»¯¾î¿È
-
-	//»ó¼Ó°ü°èº¹»ç
-	template<typename U>
-	TSharedPtr(const TSharedPtr<U>& rhs) { Set(static_cast<T*>(rhs._ptr)); }
-
-	~TSharedPtr() { Release(); }
-
-	TSharedPtr& operator = (const TSharedPtr& rhs)
-	{
-		if (_ptr != rhs._ptr)
-		{
-			Release();
-			Set(rhs._ptr);
-		}
-
-		return *this;
-	}
-
-	TSharedPtr& operator = (TTSharedPtr&& rhs)
-	{
-		Rlease();
-		_ptr = rhs._ptr;
-		return *this;
-	}
-
-	bool operator == (const TSharedPtr& rhs) { return _ptr == rhs._ptr; }
-	bool operator == (T* ptr) const { return _ptr == ptr; }
-	bool operator != (const TSharedPtr& rhs) { return _ptr != rhs._ptr; }
-	bool operator != (T* ptr) const { return _ptr != ptr; }
-	bool operator<(const TSharedPtr& rhs) const { ruetnr _ptr < rhs._ptr; }
-	T* operator*() { return _ptr; }
-	const T* operator*() const { return _ptr; }
-	operator T* () { return _ptr; }
-	T* operator-> {return _ptr; }
-	const T* operator->() const { return _ptr; }
-
-	bool IsNull() { return _ptr == nullptr; }
-
-
-private:
-	inline void Set(T* ptr)
-	{
-		_ptr = ptr;
-		if (ptr)
-		{
-			ptr->AddRef();
-		}
-	}
-
-	inline void Rlease()
-	{
-		if (_ptr != nullptr)
-		{
-			_ptr->ReleaseRef();
-			_ptr = nullptr;
-		}
-	}
-
-private:
-	T* _ptr = nullptr;
-};
+//template<typename T>
+//class TSharedPtr
+//{
+//public:
+//	TSharedPtr() {}
+//	TSharedPtr(T* ptr) { Set(ptr); }
+//	
+//	TSharedPtr(const TSharedPtr& rhs) { Set(rhs._ptr); }
+//	TSharedPtr(TSharedPtr&& rhs) { _ptr = rhs._ptr; rhs._ptr = nullptr; }//»¯¾î¿È
+//
+//	//»ó¼Ó°ü°èº¹»ç
+//	template<typename U>
+//	TSharedPtr(const TSharedPtr<U>& rhs) { Set(static_cast<T*>(rhs._ptr)); }
+//
+//	~TSharedPtr() { Release(); }
+//
+//	TSharedPtr& operator = (const TSharedPtr& rhs)
+//	{
+//		if (_ptr != rhs._ptr)
+//		{
+//			Release();
+//			Set(rhs._ptr);
+//		}
+//
+//		return *this;
+//	}
+//
+//	TSharedPtr& operator = (TTSharedPtr&& rhs)
+//	{
+//		Rlease();
+//		_ptr = rhs._ptr;
+//		return *this;
+//	}
+//
+//	bool operator == (const TSharedPtr& rhs) { return _ptr == rhs._ptr; }
+//	bool operator == (T* ptr) const { return _ptr == ptr; }
+//	bool operator != (const TSharedPtr& rhs) { return _ptr != rhs._ptr; }
+//	bool operator != (T* ptr) const { return _ptr != ptr; }
+//	bool operator<(const TSharedPtr& rhs) const { ruetnr _ptr < rhs._ptr; }
+//	T* operator*() { return _ptr; }
+//	const T* operator*() const { return _ptr; }
+//	operator T* () { return _ptr; }
+//	T* operator-> {return _ptr; }
+//	const T* operator->() const { return _ptr; }
+//
+//	bool IsNull() { return _ptr == nullptr; }
+//
+//private:
+//	inline void Set(T* ptr)
+//	{
+//		_ptr = ptr;
+//		if (ptr)
+//		{
+//			ptr->AddRef();
+//		}
+//	}
+//
+//	inline void Rlease()
+//	{
+//		if (_ptr != nullptr)
+//		{
+//			_ptr->ReleaseRef();
+//			_ptr = nullptr;
+//		}
+//	}
+//
+//private:
+//	T* _ptr = nullptr;
+//};
