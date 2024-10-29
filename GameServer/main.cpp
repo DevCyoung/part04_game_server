bf1386e22c0ec1dc5f553ab88afba5c6ed98a9b3
DevCYoung <tkdlqldjq123@gmail.com>
2024-10-30 00:21:24 +0900

    Allocator

--- GameServer/main.cpp
+++ GameServer/main.cpp
@@ -9,39 +9,70 @@
 #include "ConcurrentQueue.h"
 #include "ThreadManager.h"
 #include "Lock.h"
+#include "Memory.h"
 
 //CoreGlobal GCore;
 
 class Player
 {
+public:
+	Player(int32 other)
+	{
 
+	}
+	virtual void Pla() {};
+};
+
+class Player2 : public Player
+{
+	virtual void Pla() override {};
 };
 
 //_CrtDumpMemoryLeaks();
 
+//void* operator new(size_t size)
+//{
+//	std::cout << "new! " << size << endl;
+//	void* ptr = ::malloc(size);
+//	return ptr;
+//}
+//
+//void operator delete(void* ptr)
+//{
+//	std::cout << "delete!" << std::endl;
+//	::free(ptr);
+//}
+//
+//void* operator new[](size_t size)
+//{
+//	std::cout << "new[]! " << size << endl;
+//	void* ptr = ::malloc(size);
+//	return ptr;
+//}
+//
+//void operator delete[](void* ptr)
+//{
+//	std::cout << "delete[]!" << std::endl;
+//	::free(ptr);
+//}
+
+
+#define ynew(CLASS) (decltype(CLASS)*)new(BaseAllocator::Alloc(sizeof(decltype(CLASS))))CLASS
+#define ynewa(CLASS) (decltype(CLASS)*)new(BaseAllocator::Alloc(sizeof(decltype(CLASS))))CLASS
+#define ydeletea(pointer) BaseAllocator::Rlease(pointer)
+//#define ynew(CLASS) new CLASS
+
 int main()
 {
-	using type = remove_extent_t<int32[123]>;
+	//xnew<Player>(123);
 
-	//Shared, Weak
-	//[T*][ReCountBlocking*]
-	//조금더 우아한 방법
-	// 
+	//void* _memory = new(::malloc(sizeof(decltype(Player(32)))))Player(32);
 
-	// [T* | ReCountBlocking*] 같은공간에 낑겨넣는다.
-	std::shared_ptr<Player> spr3(new Player);
-	std::shared_ptr<Player> spr = make_shared<Player>();
-	std::weak_ptr<Player> wpr = spr;
 
-	//존재하는지 체크해야함
-	bool expired =  wpr.expired();
+	//Player* player = ynew(Player[24]);
 
-	std::shared_ptr<Player> spr2 = wpr.lock(); //없으면(sharedCount가 0이면) null 상태가 됨
-	//weakCount가 0이 아니면 RefCountingBlock은 아직 안날라간다.
+	//ydelete(player);	
 
-	//[T*][ReCountBlocking*] 동일 한 레퍼런스 동일판 포인터 블럭
-	std::shared_ptr<Player> spr2(new Player);
+	new int[124];
 
-	//Shared 와 Weak는 서로 상충
-	//Weak는 Night 수명과는 연관이없다.
 }
