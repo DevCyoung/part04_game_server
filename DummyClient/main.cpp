6c03e9999b71192b668f93b06beb475b0453e80e
DevCYoung <tkdlqldjq123@gmail.com>
2024-11-06 00:42:55 +0900

    UDP

--- DummyClient/main.cpp
+++ DummyClient/main.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <thread>
 #include <chrono>
 #include <iostream>
@@ -13,8 +14,8 @@ int main()
 	WSADATA wsaData;
 	int err = ::WSAStartup(wVersionRequested, &wsaData);
 
-	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, NULL);
-	if (listenSocket == INVALID_SOCKET)
+	SOCKET socket = ::socket(AF_INET, SOCK_DGRAM, NULL);
+	if (socket == INVALID_SOCKET)
 	{
 		int errCode = ::WSAGetLastError();
 		std::cout << errCode << " : INVALID_SOCKET" << std::endl;
@@ -25,77 +26,52 @@ int main()
 	::memset(&service, 0, sizeof(SOCKADDR_IN));
 	service.sin_family = AF_INET;
 	::inet_pton(AF_INET, "127.0.0.1", &service.sin_addr);
-	//service.sin_addr.s_addr = ::htonl(INADDR_ANY); // 니가 알아서 골라줘 말이되는주소가 연결됨
-	service.sin_port = htons(7878); //host to network short 엔디안 이슈
+	service.sin_port = htons(7899); //host to network short 엔디안 이슈
 
-	if (SOCKET_ERROR == ::bind(listenSocket, (const sockaddr*)&service, sizeof(service)))
+	if (SOCKET_ERROR == ::bind(socket, (const sockaddr*)&service, sizeof(service)))
 	{
 		int errCode = ::WSAGetLastError();
 		std::cout << errCode << " : Connect Fail" << std::endl;
 		return 0;
 	}
 
-	//영업시작 백로그는 대기열
-	::listen(listenSocket, 10);
+	SOCKADDR_IN other;
+	int tolen = sizeof(other);
+	std::vector<int> v;
 
-	//손님을 받을수있는 상태암 여기서부터 커넥트가능	
-
-	while (true)
+	while (1)
 	{
-		SOCKADDR_IN clientAddr; // IPv4
-		int addLen = sizeof(SOCKADDR_IN);
-		memset(&clientAddr, 0, sizeof(clientAddr));
-		//클라이언트의 정보를 얻을 수 있음
-		//옵션임 넣어주지 않아도 상관없음
+		int recv[10000];
+		int readlen = ::recvfrom(socket, (char*)&recv, sizeof(recv), 0, (sockaddr*)&other, &tolen);
 
-		SOCKET clientSocket = ::accept(listenSocket, (sockaddr*)&clientAddr, &addLen);
-
-		if (INVALID_SOCKET == clientSocket)
+		if (readlen == 0)
 		{
-			int errCode = ::WSAGetLastError();
-			std::cout << errCode << " : Accept Fail" << std::endl;
-			return 0;
+			std::cout << "break" << std::endl;
+			break;
 		}
-		std::cout << "Accept Client !" << std::endl;
 
-		//손님 입장
-		char ipAddress[16];
-		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
-		std::cout << "Client Connected! IP = " << ipAddress << std::endl;
+		v.push_back(recv[0]);
 
-		//Todo
-
-		while (true)
-		{
-			//std::this_thread::sleep_for(std::chrono::seconds(10));
-
-			char recvBuffer[256];
-
-			int recvlen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-
-			if (recvlen == SOCKET_ERROR)
-			{
-				std::cout << "Send Fail" << std::endl;
-			}
-
-			std::cout << "Recv Data! Len = " << recvlen << std::endl;
-			std::cout << "Recv Data! = " << recvBuffer << std::endl;
-
-			strcat_s(recvBuffer, "[Re: Server]");
-
-			int sendlen = ::send(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-
-			if (sendlen == SOCKET_ERROR)
-			{
-				std::cout << "Send Fail" << std::endl;
-			}
-
-			std::cout << "Send Data! Len = " << sendlen << std::endl;
-			std::cout << "Send Data = " << recvBuffer << std::endl;
-
-			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-		}		
+		//std::cout << readlen << " r " << recv[0] << " " << v.size() * sizeof(int) << std::endl;
 	}
+
+	
+
+	int def = 0;
+	for (int i = 1; i < v.size(); ++i)
+	{
+		if (v[i] - v[i - 1] != 1)
+		{
+			int v1 = v[i];
+			int v0 = v[i - 1];
+			++def;
+			continue;
+		}
+	}
+
+	std::cout << "Def " << def << std::endl;
+
+	while (true);
 
 	::WSACleanup();
 }
--- GameServer/main.cpp
+++ GameServer/main.cpp
@@ -17,10 +17,7 @@ int main()
 	WSADATA wsaData;
 	int err = ::WSAStartup(wVersionRequested, &wsaData);
 	
-	//TCP
-	//굳이적지않아도 TCP UDP를, 0으로 입력하면 알아서 골라줌
-	//소켓은 포인터일뿐임
-	SOCKET socket = ::socket(AF_INET, SOCK_STREAM, NULL);
+	SOCKET socket = ::socket(AF_INET, SOCK_DGRAM, NULL);
 	if (socket == INVALID_SOCKET)
 	{
 		int errCode = ::WSAGetLastError();
@@ -30,74 +27,53 @@ int main()
 
 	SOCKADDR_IN service;
 	::memset(&service, 0, sizeof(SOCKADDR_IN));
-	service.sin_family = AF_INET;
-	//service.sin_addr.s_addr = inet_addr("127.0.0.1"); // 그냥 4바이트정수 inet_addr은 구식함수
-	//naver.com
+	service.sin_family = AF_INET;	
 	::inet_pton(AF_INET, "127.0.0.1", &service.sin_addr);
 	service.sin_port = htons(7878); //host to network short 엔디안 이슈
-	//Littl-Endian vs Big-Endian
-	// 0x12345678
-	// low [78][56][34][12] 리틀엔디안
-	// low [12][34][56][78] 빅엔디안 = 네트워크 표준 중간중간 장비들도 기계다보니까 두가지 방식중 하나를 골라서 사용해야함
-	// 때문에 한가지정책을 골라서 일관성있게 사용해야한다.
-	// 대부분은 리틀엔디안
 
-
-	//버전이 여러개기 때문에 이렇게하는것
-	if (SOCKET_ERROR == ::connect(socket, (struct sockaddr*)&service, sizeof(SOCKADDR_IN)))
+	if (SOCKET_ERROR == ::bind(socket, (const sockaddr*)&service, sizeof(service)))
 	{
 		int errCode = ::WSAGetLastError();
 		std::cout << errCode << " : Connect Fail" << std::endl;
 		return 0;
 	}
 
-	std::cout << "Connected To Server !" << std::endl;
 
-	while (true)
+	//other
+	SOCKADDR_IN other;
+	::memset(&other, 0, sizeof(SOCKADDR_IN));
+	other.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1", &other.sin_addr);
+	other.sin_port = htons(7899); //host to network short 엔디안 이슈
+	int pa[10000];
+
+	int& a = pa[0];
+	a = 0;
+
+	while (1)
 	{
-		//std::this_thread::sleep_for(std::chrono::seconds(10));
-
-		char recvBuffer[256] = "Hello World!";				
+		int sendlen = ::sendto(socket, (const char*)pa, sizeof(pa), 0, (sockaddr*)&other, sizeof(other));
+		//std::cout << sendlen << " s " << a  <<std::endl;
 		
-		
-
-		int sendlen = ::send(socket, recvBuffer, sizeof(recvBuffer), 0);
-
-		if (sendlen == SOCKET_ERROR)
+		if (a >= 10000)
 		{
-			std::cout << "Send Fail" << std::endl;
+			::sendto(socket, (const char*)&pa, 0, 0, (sockaddr*)&other, sizeof(other));
+			//std::cout << "sendto0" << std::endl;
+			break;
 		}
 
-		std::cout << "Send Data! Len = " << sendlen << std::endl;
-		std::cout << "Send Data = " << recvBuffer << std::endl;
-
-		shutdown(socket, SD_RECEIVE);
-
-		int recvlen = ::recv(socket, recvBuffer, sizeof(recvBuffer), 0);
-
-		if (recvlen == SOCKET_ERROR)
-		{
-			std::cout << "Send Fail" << std::endl;
-		}
-
-		std::cout << "Recv Data! Len = " << recvlen << std::endl;
-		std::cout << "Recv Data! = " << recvBuffer << std::endl;
-
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		++a;
 	}
 
 
+	::sendto(socket, (const char*)&pa, 0, 0, (sockaddr*)&other, sizeof(other));
 
-	/*if (SOCKET_ERROR == ::bind(socket, (struct sockaddr*)&service, sizeof(SOCKADDR_IN)))
-	{
-		int32 errCode = ::WSAGetLastError();
-		std::cout << errCode << " : SOCKET_ERROR" << std::endl;
-		return 0;
-	}	*/
+	std::cout << a << std::endl;
+
 	
 
-	//::bind(socket, )
+	while (true);
+
 
 	::WSACleanup();
 }
