34cb37f62d630f21faff4b2f2b0a19aa6e98d540
DevCYoung <tkdlqldjq123@gmail.com>
2024-11-05 22:38:52 +0900

    Socket

--- DummyClient/main.cpp
+++ DummyClient/main.cpp
@@ -1,7 +1,73 @@
-#include "pch.h"
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-	HelloDummyClient();
-	return 0;
+	//Winsock lib 초기화
+	WORD wVersionRequested = MAKEWORD(2, 2);
+	WSADATA wsaData;
+	int err = ::WSAStartup(wVersionRequested, &wsaData);
+
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, NULL);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		int errCode = ::WSAGetLastError();
+		std::cout << errCode << " : INVALID_SOCKET" << std::endl;
+		return 0;
+	}
+
+	SOCKADDR_IN service;
+	::memset(&service, 0, sizeof(SOCKADDR_IN));
+	service.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1", &service.sin_addr);
+	//service.sin_addr.s_addr = ::htonl(INADDR_ANY); // 니가 알아서 골라줘 말이되는주소가 연결됨
+	service.sin_port = htons(7878); //host to network short 엔디안 이슈
+
+	if (SOCKET_ERROR == ::bind(listenSocket, (const sockaddr*)&service, sizeof(service)))
+	{
+		int errCode = ::WSAGetLastError();
+		std::cout << errCode << " : Connect Fail" << std::endl;
+		return 0;
+	}
+
+	//영업시작 백로그는 대기열
+	::listen(listenSocket, 10);
+
+	//손님을 받을수있는 상태암 여기서부터 커넥트가능	
+
+	while (true)
+	{
+		SOCKADDR_IN clientAddr; // IPv4
+		int addLen = sizeof(SOCKADDR_IN);
+		memset(&clientAddr, 0, sizeof(clientAddr));
+		//클라이언트의 정보를 얻을 수 있음
+		//옵션임 넣어주지 않아도 상관없음
+
+		SOCKET clientSocket = ::accept(listenSocket, (sockaddr*)&clientAddr, &addLen);
+
+		if (INVALID_SOCKET == clientSocket)
+		{
+			int errCode = ::WSAGetLastError();
+			std::cout << errCode << " : Accept Fail" << std::endl;
+			return 0;
+		}
+		std::cout << "Accept Client !" << std::endl;
+
+		//손님 입장
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		std::cout << "Client Connected! IP = " << ipAddress << std::endl;
+
+		//Todo
+
+
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+
+	::WSACleanup();
 }
--- GameServer/main.cpp
+++ GameServer/main.cpp
@@ -1,69 +1,74 @@
-#include "pch.h"
+//#include "pch.h"
 #include <thread>
-#include <atomic>
-#include <mutex>
-#include <Windows.h>
-#include <assert.h>
-#include <future>
-#include "ConcurrentStack.h"
-#include "ConcurrentQueue.h"
-#include "ThreadManager.h"
-#include "Lock.h"
-#include "Memory.h"
-#include "TypeCast.h"
+#include <chrono>
+#include <iostream>
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
-using TL = TypeList<class Player, class Charactor, class Knight, class  QD>;
 
-class Player
-{
-public:
-	Player()
-	{
-		INIT_TL(Player);
-	}
 
-	DECLARE_TL;
-};
-
-class Charactor : public Player
-{
-public:
-	Charactor() { INIT_TL(Charactor); };
-};
-
-class Knight : public Player
-{
-public:
-	Knight() { INIT_TL(Knight); }
-};
-
-class QD
-{
-
-};
 
 int main()
 {
-	Player* player = new Player();
-
-	Knight* knight = TypeCast<Knight*>(player);
-
-	if (knight)
+	//Winsock lib 초기화
+	WORD wVersionRequested = MAKEWORD(2, 2);
+	WSADATA wsaData;
+	int err = ::WSAStartup(wVersionRequested, &wsaData);
+	
+	//TCP
+	//굳이적지않아도 TCP UDP를, 0으로 입력하면 알아서 골라줌
+	//소켓은 포인터일뿐임
+	SOCKET socket = ::socket(AF_INET, SOCK_STREAM, NULL);
+	if (socket == INVALID_SOCKET)
 	{
-
+		int errCode = ::WSAGetLastError();
+		std::cout << errCode << " : INVALID_SOCKET" << std::endl;
+		return 0;
 	}
 
-	Player* p = TypeCast<Player*>(knight);
+	SOCKADDR_IN service;
+	::memset(&service, 0, sizeof(SOCKADDR_IN));
+	service.sin_family = AF_INET;
+	//service.sin_addr.s_addr = inet_addr("127.0.0.1"); // 그냥 4바이트정수 inet_addr은 구식함수
+	//naver.com
+	::inet_pton(AF_INET, "127.0.0.1", &service.sin_addr);
+	service.sin_port = htons(7878); //host to network short 엔디안 이슈
+	//Littl-Endian vs Big-Endian
+	// 0x12345678
+	// low [78][56][34][12] 리틀엔디안
+	// low [12][34][56][78] 빅엔디안 = 네트워크 표준 중간중간 장비들도 기계다보니까 두가지 방식중 하나를 골라서 사용해야함
+	// 때문에 한가지정책을 골라서 일관성있게 사용해야한다.
+	// 대부분은 리틀엔디안
 
 
-	Player* knight2 = new Knight();
-
-
-	bool can = CanCast<Knight*>(knight2);
-
-
-	if (knight)
+	//버전이 여러개기 때문에 이렇게하는것
+	if (SOCKET_ERROR == ::connect(socket, (struct sockaddr*)&service, sizeof(SOCKADDR_IN)))
 	{
-
+		int errCode = ::WSAGetLastError();
+		std::cout << errCode << " : Connect Fail" << std::endl;
+		return 0;
 	}
+
+	std::cout << "Connected To Server !" << std::endl;
+
+	while (true)
+	{
+		//std::this_thread::sleep_for(1ms);
+	}
+
+
+
+	/*if (SOCKET_ERROR == ::bind(socket, (struct sockaddr*)&service, sizeof(SOCKADDR_IN)))
+	{
+		int32 errCode = ::WSAGetLastError();
+		std::cout << errCode << " : SOCKET_ERROR" << std::endl;
+		return 0;
+	}	*/
+	
+
+	//::bind(socket, )
+
+	::WSACleanup();
 }
